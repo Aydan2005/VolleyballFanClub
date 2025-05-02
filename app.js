f828d756459dd28e886f26883e372c5a6c482bad
Aydan2005 <mehtiyevaaydan308@gmail.com>
2025-05-02 17:26:05 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const track = document.querySelector(".slider-track");
+const dots = document.querySelectorAll(".dot");
+let index = 0;
+const totalSlides = document.querySelectorAll(".slide").length;
+
+function updateSlider() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach(dot => dot.classList.remove("active-dot"));
+  dots[index].classList.add("active-dot");
+}
+
+function nextSlide() {
+  index = (index + 1) % totalSlides;
+  updateSlider();
+}
+
+setInterval(nextSlide, 5000);
+
+
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
+const rightIcons = document.querySelector('.right-icons');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  rightIcons.classList.toggle('active');
+});
